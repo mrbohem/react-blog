4df4b45f4d1b5f68767b0efac33af2ceaf1ed823
Shivam Upadhyay <09sh27up@gmail.com>
2023-04-12 12:50:23 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import UserSearch from './classes/UserSearch';
+import Parent from './props/Parent';
+import RefHook from './hooks/useRef';
+import LoginForm from './pages/LoginForm';
+import storage from './utils/storage';
+import axios, { AxiosResponse } from 'axios';
+import { useDispatch } from 'react-redux';
+import { Route, Router, Routes, useNavigate } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Post from './pages/Post';
 
 function App() {
+  // const [token, setToken] = useState<string | null>(null);
+  const dispatch = useDispatch()
+  const navigate = useNavigate();
+
+
+    useEffect(() =>{
+
+      const getMe = async () => {
+        await axios.post('http://localhost:3001/me','',{
+          headers: {
+            authorization: `bearer ${token}`,
+          },
+        })
+        .then((response: AxiosResponse) => {
+          dispatch({type:"user_logged_in",payload:response.data})
+          axios.defaults.headers.common['authorization'] = `bearer ${token}`
+        })
+        .catch((error: Error) => {
+          storage.clearToken()
+          navigate('/')
+        })
+      }
+      let token = storage.getToken()
+      if(token){
+        getMe()
+      }
+      else{
+        navigate('/')
+      }
+    },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<LoginForm />} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/post' element={<Post/>} />
+    </Routes>
+    // <div className="App">
+    //   <Parent />
+    //   <UserSearch users={users}/>
+    //   <RefHook />
+    // </div>
+    
   );
 }
 
